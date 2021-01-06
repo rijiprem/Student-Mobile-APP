2ff350e9a781993fee94af7e757a27cc97a067e8
rijiprem <rijir.kmr@gmail.com>
2021-01-06 23:33:35 +0530

    read the datas and displayed

--- app/src/main/java/com/riji/studentapp/MainActivity.kt
+++ app/src/main/java/com/riji/studentapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.riji.studentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun submit(view: View) {
+        var getStudentName=findViewById<EditText>(R.id.sname);
+        var getAdmissionNumber=findViewById<EditText>(R.id.adnum);
+        var getRollNumber=findViewById<EditText>(R.id.roll);
+        var getCollegeName=findViewById<EditText>(R.id.college);
+        Toast.makeText(this,getStudentName.text.toString()+"  "+
+        getAdmissionNumber.text.toString()+"  "+getRollNumber.text.toString()+"  "+
+        getCollegeName.text.toString(),Toast.LENGTH_LONG).show();
     }
 }
